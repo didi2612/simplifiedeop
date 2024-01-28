e1bcf506877a68200b8c24e9487885a03194eeed
didi2612 <github@azmiproductions.com>
2024-01-29 00:21:46 +0800

    modified:   mengwritingfile.java

--- mengwritingfile.java
+++ mengwritingfile.java
@@ -1,10 +1,27 @@
 package practice;
-import java.util.Scanner;
 import java.io.FileNotFoundException;
 import java.io.File;
-
-public class mengwritingfile {
-    public static void main(String[] args){
-
-    }
-}
+import java.util.Scanner;
+ 
+ 
+ public class mengwritingfile {
+     public static void main(String[] args) {
+ 
+         File file = new File("EOP.txt");
+         
+         // Create a Scanner object through the File object, and use it as usual
+         try(Scanner input = new Scanner(file)){
+ 
+             // hasNext() and hasNextLine() would be useful  
+             while(input.hasNextLine()){
+                 System.out.println(input.nextLine());
+             }            
+             System.out.println("\nThe file has been successfully read!");
+         }
+         
+         catch(FileNotFoundException ex) {
+             System.out.println("File is not there, or could not open successfully..");
+         }
+     }    
+ }
+ 
